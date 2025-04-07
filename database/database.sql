e5271fe968c825837d754c9f137efbf0779420a8
=kaido <=kolatd51@gmail.com>
2025-04-07 12:09:52 +0200

    bd4

--- database/database.sql
+++ database/database.sql
@@ -1,102 +1,165 @@
-CREATE TABLE utilisateur ( 
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 07, 2025 at 10:08 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
-   id_user SMALLINT AUTO_INCREMENT, 
-   email VARCHAR(50) NOT NULL UNIQUE, 
-   role ENUM('user', 'admin') NOT NULL, 
-   password CHAR(60) NOT NULL,  
-   PRIMARY KEY(id_user) 
-); 
-
- 
-
-CREATE TABLE ingredient ( 
-
-   id_ingredient SMALLINT AUTO_INCREMENT, 
-   nom VARCHAR(40) NOT NULL, 
-   mesure VARCHAR(20) NOT NULL,    
-   PRIMARY KEY(id_ingredient) 
-
-); 
-
- 
-
-CREATE TABLE recette ( 
-
-   id_recette SMALLINT AUTO_INCREMENT, 
-   nom VARCHAR(60) NOT NULL, 
-   tmp_preparation TINYINT UNSIGNED NOT NULL,  -- en minutes 
-   tmp_cuisson TINYINT UNSIGNED,               -- aussi en minutes 
-   nbr_personne TINYINT UNSIGNED NOT NULL, 
-   origine VARCHAR(40), 
-   image VARCHAR(255), 
-   PRIMARY KEY(id_recette) 
-
-); 
-
- 
-
-CREATE TABLE ustensil ( 
-
-   id_ustensil TINYINT AUTO_INCREMENT, 
-   nom VARCHAR(40) NOT NULL, 
-   PRIMARY KEY(id_ustensil) 
-); 
-
- 
-
-CREATE TABLE etape ( 
-
-   id_etape MEDIUMINT AUTO_INCREMENT, 
-   numero TINYINT UNSIGNED NOT NULL, 
-   description VARCHAR(255) NOT NULL, 
-   id_recette SMALLINT NOT NULL, 
-   PRIMARY KEY(id_etape), 
-   FOREIGN KEY(id_recette) REFERENCES recette(id_recette) 
-); 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE rating ( 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-   id_rating INT AUTO_INCREMENT, 
-   note DECIMAL(3,2) NOT NULL CHECK (note BETWEEN 0 AND 5), 
-   commentaire VARCHAR(255), 
-   id_user SMALLINT NOT NULL, 
-   id_recette SMALLINT NOT NULL, 
-   PRIMARY KEY(id_rating), 
-   FOREIGN KEY(id_user) REFERENCES utilisateur(id_user), 
-   FOREIGN KEY(id_recette) REFERENCES recette(id_recette) 
+--
+-- Database: `recettes`
+--
 
-); 
+-- --------------------------------------------------------
 
-CREATE TABLE user_recette ( 
+--
+-- Table structure for table `etape`
+--
 
-   id_user SMALLINT, 
-   id_recette SMALLINT, 
-   PRIMARY KEY(id_user, id_recette), 
-   FOREIGN KEY(id_user) REFERENCES utilisateur(id_user), 
-   FOREIGN KEY(id_recette) REFERENCES recette(id_recette) 
-); 
+DROP TABLE IF EXISTS `etape`;
+CREATE TABLE IF NOT EXISTS `etape` (
+  `id_etape` mediumint NOT NULL AUTO_INCREMENT,
+  `numero` tinyint UNSIGNED NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `id_recette` smallint NOT NULL,
+  PRIMARY KEY (`id_etape`),
+  KEY `id_recette` (`id_recette`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
- 
+-- --------------------------------------------------------
 
-CREATE TABLE ingredient_recette ( 
+--
+-- Table structure for table `ingredient`
+--
 
-   id_ingredient SMALLINT, 
-   id_recette SMALLINT, 
-   quantite DECIMAL(5,2),  -- Pour stocker la quantité 
-   PRIMARY KEY(id_ingredient, id_recette), 
-   FOREIGN KEY(id_ingredient) REFERENCES ingredient(id_ingredient), 
-   FOREIGN KEY(id_recette) REFERENCES recette(id_recette) 
-); 
+DROP TABLE IF EXISTS `ingredient`;
+CREATE TABLE IF NOT EXISTS `ingredient` (
+  `id_ingredient` smallint NOT NULL AUTO_INCREMENT,
+  `nom` varchar(40) NOT NULL,
+  `mesure` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_ingredient`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
- 
+-- --------------------------------------------------------
 
-CREATE TABLE ustensil_recette ( 
+--
+-- Table structure for table `ingredient_recette`
+--
 
-   id_recette SMALLINT, 
-   id_ustensil TINYINT, 
-   PRIMARY KEY(id_recette, id_ustensil), 
-   FOREIGN KEY(id_recette) REFERENCES recette(id_recette), 
-   FOREIGN KEY(id_ustensil) REFERENCES ustensil(id_ustensil) 
+DROP TABLE IF EXISTS `ingredient_recette`;
+CREATE TABLE IF NOT EXISTS `ingredient_recette` (
+  `id_ingredient` smallint NOT NULL,
+  `id_recette` smallint NOT NULL,
+  `quantite` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id_ingredient`,`id_recette`),
+  KEY `id_recette` (`id_recette`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-); 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+CREATE TABLE IF NOT EXISTS `rating` (
+  `id_rating` int NOT NULL AUTO_INCREMENT,
+  `note` decimal(3,2) NOT NULL,
+  `commentaire` varchar(255) DEFAULT NULL,
+  `id_user` smallint NOT NULL,
+  `id_recette` smallint NOT NULL,
+  PRIMARY KEY (`id_rating`),
+  KEY `id_user` (`id_user`),
+  KEY `id_recette` (`id_recette`)
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recette`
+--
+
+DROP TABLE IF EXISTS `recette`;
+CREATE TABLE IF NOT EXISTS `recette` (
+  `id_recette` smallint NOT NULL AUTO_INCREMENT,
+  `nom` varchar(60) NOT NULL,
+  `tmp_preparation` tinyint UNSIGNED NOT NULL,
+  `tmp_cuisson` tinyint UNSIGNED DEFAULT NULL,
+  `nbr_personne` tinyint UNSIGNED NOT NULL,
+  `origine` varchar(40) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_recette`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_recette`
+--
+
+DROP TABLE IF EXISTS `user_recette`;
+CREATE TABLE IF NOT EXISTS `user_recette` (
+  `id_user` smallint NOT NULL,
+  `id_recette` smallint NOT NULL,
+  PRIMARY KEY (`id_user`,`id_recette`),
+  KEY `id_recette` (`id_recette`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ustensil`
+--
+
+DROP TABLE IF EXISTS `ustensil`;
+CREATE TABLE IF NOT EXISTS `ustensil` (
+  `id_ustensil` tinyint NOT NULL AUTO_INCREMENT,
+  `nom` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_ustensil`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ustensil_recette`
+--
+
+DROP TABLE IF EXISTS `ustensil_recette`;
+CREATE TABLE IF NOT EXISTS `ustensil_recette` (
+  `id_recette` smallint NOT NULL,
+  `id_ustensil` tinyint NOT NULL,
+  PRIMARY KEY (`id_recette`,`id_ustensil`),
+  KEY `id_ustensil` (`id_ustensil`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_user` smallint NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `role` enum('user','admin') NOT NULL,
+  `password` char(60) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
